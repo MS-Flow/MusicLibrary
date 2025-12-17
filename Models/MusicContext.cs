f4d8458786c54b70fc630606ccd3cdb024094728
sebastianbreuker <sebbebreuker@gmail.com>
2025-12-17 11:19:31 +0100

    Changed the names from Swedish to English

--- Models/MusicContext.cs
+++ Models/MusicContext.cs
@@ -31,7 +31,7 @@ public partial class MusicContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=everyloop;User ID=sa;Password=Password123!;TrustServerCertificate=True;MultipleActiveResultSets=True");
+        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=everyloop;TrustServerCertificate=True;Integrated Security=True;MultipleActiveResultSets=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
